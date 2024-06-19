59efffad09ea057efec239d502e302be89a23d30
unknown <APRENDIZ@BOGDAFCGFFSPP230.FormacionSena.RED>
2024-06-18 21:24:53 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import routes from'./config/routes.js'
+
+const app= express()
+const corsOption={
+    origin:'*'
+}
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use('/api',cors(corsOption), routes)
+
+app.get('/',(req,res)=> res.send('Estamos en el servidor'))
+
+const server=app.listen(process.env.PORT || 8000,()=>{
+    console.log('Etamos en JAPÓN')
+})
